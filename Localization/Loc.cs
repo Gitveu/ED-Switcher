b99d56d5e1c6dac3b4dc258969d99c1eb664a8a7
Gitveu <panteleenko8@gmail.com>
2026-08-22 22:51:55 +0300

    feat: Loc markup extension for XAML strings

---
+++ Localization/Loc.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.UI.Xaml.Markup;
+
+namespace EDAccountSwitcher.Localization
+{
+    [MarkupExtensionReturnType(ReturnType = typeof(string))]
+    public sealed class Loc : MarkupExtension
+    {
+        public string Key { get; set; }
+
+        protected override object ProvideValue() => LocalizationManager.Get(Key);
+    }
+}
