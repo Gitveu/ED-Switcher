5897343bb129c85cd0a06b2ca2c286e4ec7c8423
Gitveu <panteleenko8@gmail.com>
2026-08-22 22:52:50 +0300

    test: disable parallel execution (static state)

---
+++ tests/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using Xunit;
+
+// SettingsStore, SoundHelper and LocalizationManager hold static state,
+// so test classes must not run in parallel.
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
