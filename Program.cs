f5a8a0bb9c822fca86a16a98d9ce88080483c778
Gitveu <panteleenko8@gmail.com>
2026-09-15 19:01:01 +0300

    Add custom entry point for updater mode

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using EDAccountSwitcher.Updating;
+
+namespace EDAccountSwitcher
+{
+    public static class Program
+    {
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            if (args.Length > 0 && string.Equals(args[0], UpdateService.ApplySwitch, StringComparison.OrdinalIgnoreCase))
+            {
+                Environment.ExitCode = UpdateService.ApplyStagedUpdate(args);
+                return;
+            }
+
+            UpdateService.CleanupAfterUpdate();  
+            global::WinRT.ComWrappersSupport.InitializeComWrappers();
+            global::Microsoft.UI.Xaml.Application.Start(_ =>
+            {
+                var context = new global::Microsoft.UI.Dispatching.DispatcherQueueSynchronizationContext(
+                    global::Microsoft.UI.Dispatching.DispatcherQueue.GetForCurrentThread());
+                global::System.Threading.SynchronizationContext.SetSynchronizationContext(context);
+                new App();
+            });
+        }
+    }
+}
